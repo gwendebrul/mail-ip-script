5b3dec616093f9d1b676902d5abd6147fdc55d32
Gwen De Brul <gwen@debrul.be>
2023-12-01 09:30:25 +0100

    Updated the curl operation and the ifttt url. Now you can use this script without jsonPayload, but with parameters like 'value1' etc.

--- mail-ip.sh
+++ mail-ip.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+## Change the following variables
+## FIRSTNUMBER is the first number of the ip address in your network
+## SO 192.168.1.251 gives 192 as FIRSTNUMBER
+
+## IFTTURL must be changed with your event name and your key
 FIRSTNUMBER="192"
-IFTTURL="https://maker.ifttt.com/trigger/<your_event>/json/with/key/<your_key>"
+IFTTURL="https://maker.ifttt.com/trigger/<your_event>/with/key/<your_key>"
+
+## Don't change anything after this line
 
 IP=$(ip a | grep 'inet' | grep -E -o "$FIRSTNUMBER\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | head -1)
 HOSTNAME=$(hostname)
 
-curl -X POST -H "Content-Type: application/json" -d "{\"hostname\":\"$HOSTNAME\",\"IP\":\"$IP\"}" $IFTTURL >/dev/null 2>&1
+curl -X POST -d "{\"hostname\":\"$HOSTNAME\",\"IP\":\"$IP\"}" $IFTTURL >/dev/null 2>&1
